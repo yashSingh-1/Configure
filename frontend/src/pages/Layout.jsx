030a1fa33ca443342ae8ac9a782f2d259529fc9f
Yash Singh <dobbyneedsclothes@gmail.com>
2025-02-05 00:30:17 +0530

    fe setting up

---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div className='flex justify-between'>
+        <div>
+            Configure
+        </div>
+        <div>
+            Open
+        </div>
+    </div>
+  )
+}
+
+export default Layout
